b2c6f49bd8585a720df1cadb5a0245be9e3adce4
Faizurcse <mdfaizurrahman2000@gmail.com>
2025-03-28 01:21:33 +0530

    push

--- Socket/Socket.io.js
+++ Socket/Socket.io.js
@@ -11,7 +11,7 @@ let saveDataFlag = false;
 
 const io = new Server(server, {
   cors: {
-    origin: ["*"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
     credentials: true, // ✅ Needed if frontend uses withCredentials
   },
